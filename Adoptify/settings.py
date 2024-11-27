fcdd3311900dbec98c8de37c084cdc07fb825bd4
dibbodas12 <dibbodas228@gmail.com>
2024-11-27 12:44:29 +0600

    changed

--- Adoptify/settings.py
+++ Adoptify/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vevj86+u8ki&rvb0yp^yxk=gp%jky(l*cxl&6djbz((2-l+2e0'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
